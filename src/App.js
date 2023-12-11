140ecdac740bce9a1c863bbcfe5fe625a9666996
Prajwal0300 <prajwalmahes007@gmail.com>
2023-12-12 01:20:57 +0530

    Added ‘AddSubscriber’ component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
 import React, { Component } from 'react';
 import Header from "./Header.js"
+import './Header.css'
+import './app.css'
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <button> Add </button>
-      <div>
-        <span>Name</span><br />
-        <span>Phone</span>
+class App extends Component {
+
+  deleteHandler(message) {
+    alert(message);
+  }
+
+  // Define subscribers as a class property
+  subscribers = [
+    {
+      id:1,
+      name: "Shilpa Bhat",
+      phone: "8888888888"
+    },
+    {
+      id:2, 
+      name: "Srishti",
+      phone: "8888888888"
+    }
+  ];
+
+  render() {
+    return (
+      <div className='main'>
+        <Header heading="Phone Directory" instructor="Shilpa Bhat"/>
+        <div className='component-sub'>
+          <button className='button'>Add</button>
+
+          <div className='grid-container'>
+            <span className='item'>Name</span>
+            <span className='item'>Phone</span><br/><br/>
+          </div>
+
+          {
+            this.subscribers.map(sub => {
+              return (
+                <div className='grid-container' key={sub.id}>
+                  <span className='item'>{sub.name}</span>
+                  <span className='item'>{sub.phone}</span>
+                  <span className='item action-btn-container'>
+                    <button className='custom-btn delete-btn' onClick={this.deleteHandler.bind(this, "Delete Handler Clicked")}>Delete</button>
+                  </span>
+                </div>
+              );
+            })
+          }
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
